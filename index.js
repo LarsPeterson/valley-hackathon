a4d364ad6c22bcf790be1e0c920043450326495d
Lars Peterson <larspeterson@live.com>
2019-03-23 13:25:26 -0500

    update

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+// 1. enter password
+// 2. hit API with entered password
+// 3. return list of similar passwords (passwords that match first few characters)
+// 4. narrow list by 
+
+// user enters password
+// encrypt it (salt + hash)
+// 
+
+// barebones
+// api that checks if comprimised password
+
+const passwordToCheck = "some_random_password";
+
+const wasPasswordComprimised = function (password) {
+
+}
+
+const encryptPassword = function (password) {
+
+}
